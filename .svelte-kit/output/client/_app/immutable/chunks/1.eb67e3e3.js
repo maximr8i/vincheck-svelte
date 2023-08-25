49efd1beecb6b24ed909e2b2c7ea91e2866cc216
dyhnila <roman.dmitrievich@gmail.com>
2023-08-25 14:14:31 +0300

    Delete .svelte-kit folder

--- .svelte-kit/output/client/_app/immutable/chunks/1.eb67e3e3.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.f00270b7.js";export{t as component};
--- .svelte-kit/output/client/_app/immutable/chunks/2.74d3a718.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.a8930b57.js";export{t as component};
