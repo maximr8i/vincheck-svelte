20903c488b7388ec67dba3e1ca81157b22d3a126
dyhnila <roman.dmitrievich@gmail.com>
2023-08-28 16:55:58 +0300

    Add google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
   import '../styles/reset.scss'
   import Footer from '../components/Footer/Footer.svelte'
+  import GoogleAnalytics from '../components/GoogleAnalytics/GoogleAnalytics.svelte'
   import Header from '../components/Header/Header.svelte'
 </script>
 
+<svelte:head>
+  <GoogleAnalytics />
+</svelte:head>
 <Header />
 <main>
   <slot />
