4b9b2b39bf46b180c0154a4b5bfc84877691efe9
dyhnila <roman.dmitrievich@gmail.com>
2023-08-28 17:39:14 +0300

    Add yandex metrics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import '../styles/reset.scss'
   import GoogleAnalytics from '$lib/GoogleAnalytics/GoogleAnalytics.svelte'
+  import YandexAnalytics from '$lib/YandexAnalytics/YandexAnalytics.svelte'
   import Footer from '../components/Footer/Footer.svelte'
   import Header from '../components/Header/Header.svelte'
 </script>
 
 <GoogleAnalytics />
+<YandexAnalytics />
 <Header />
 <main>
   <slot />
