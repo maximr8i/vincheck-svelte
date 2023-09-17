69909922cc8200e82e3516a16f74ffcaad0ac79e
maximr8i <131347985+maximr8i@users.noreply.github.com>
2023-09-17 17:16:20 +0700

    Update +server.ts

--- src/routes/api/search/[vin]/+server.ts
+++ src/routes/api/search/[vin]/+server.ts
@@ -1,7 +1,12 @@
-export const GET: RequestHandler = async ({ params: { vin } }) => {
+import { RequestHandler } from '@vercel/node';
+
+export const GET: RequestHandler = async ({ params: { vin }, req }) => {
+  const xForwardedFor = req.headers['x-real-ip']
+  
   const response = await fetch(`http://84.54.47.216/v1/carlist/search/${vin}`, {
     headers: {
-      protection: 'YjWVsPQ6EM!WUaeSsydsPiWHDdp/vbg9JCNefGHltBdddPbb8md0mr=n86hzAyiv'
+      protection: 'YjWVsPQ6EM!WUaeSsydsPiWHDdp/vbg9JCNefGHltBdddPbb8md0mr=n86hzAyiv',
+      'X-Forwarded-For': xForwardedFor
     }
   })
 
